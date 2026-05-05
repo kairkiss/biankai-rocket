44d86b17d9c530d4e9a83b14b9627c2da54790a2
kairkiss <kairkiss@users.noreply.github.com>
2026-05-05 23:32:36 +0800

    Fix Android component names after package rename

--- android/common/src/main/java/com/follow/clash/common/Components.kt
+++ android/common/src/main/java/com/follow/clash/common/Components.kt
@@ -4,13 +4,14 @@ import android.content.ComponentName
 
 object Components {
     const val PACKAGE_NAME = "com.biankai.rocket"
+    private const val APP_COMPONENT_PACKAGE_NAME = "com.follow.clash"
 
     val MAIN_ACTIVITY =
-        ComponentName(GlobalState.packageName, "${PACKAGE_NAME}.MainActivity")
+        ComponentName(GlobalState.packageName, "$APP_COMPONENT_PACKAGE_NAME.MainActivity")
 
     val TEMP_ACTIVITY =
-        ComponentName(GlobalState.packageName, "${PACKAGE_NAME}.TempActivity")
+        ComponentName(GlobalState.packageName, "$APP_COMPONENT_PACKAGE_NAME.TempActivity")
 
     val BROADCAST_RECEIVER =
-        ComponentName(GlobalState.packageName, "${PACKAGE_NAME}.BroadcastReceiver")
+        ComponentName(GlobalState.packageName, "$APP_COMPONENT_PACKAGE_NAME.BroadcastReceiver")
 }
